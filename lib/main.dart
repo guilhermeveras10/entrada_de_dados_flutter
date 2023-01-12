7817e1402dfbcfc183c505213d4f91c507518ff8
Guilherme Duarte <guilherme.veras10@outlook.com>
2023-01-12 16:09:10 -0300

    radio button

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:entrada_de_dados/CampoTexto.dart';
 import 'package:entrada_de_dados/EntradaCheckbox.dart';
+import 'package:entrada_de_dados/EntradaRadioButton.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: EntradaCheckbox(),
+    home: EntradaRadioButton(),
+    // home: EntradaCheckbox(),
     // home: CampoTexto(),
   ));  
 }
