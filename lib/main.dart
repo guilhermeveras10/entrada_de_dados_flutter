e76130e710b666ff383c17e2c94cfb941a4a3c72
Guilherme Duarte <guilherme.veras10@outlook.com>
2023-01-12 15:51:45 -0300

    checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:entrada_de_dados/CampoTexto.dart';
+import 'package:entrada_de_dados/EntradaCheckbox.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: CampoTexto(),
+    home: EntradaCheckbox(),
+    // home: CampoTexto(),
   ));  
 }
