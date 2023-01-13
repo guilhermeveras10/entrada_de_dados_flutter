7ccaf64b136003f93f2e319fec8f008c7cb85525
Guilherme Duarte <guilherme.veras10@outlook.com>
2023-01-13 17:53:00 -0300

    slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:entrada_de_dados/CampoTexto.dart';
 import 'package:entrada_de_dados/EntradaCheckbox.dart';
 import 'package:entrada_de_dados/EntradaRadioButton.dart';
+import 'package:entrada_de_dados/EntradaSlider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: EntradaRadioButton(),
+    home: EntradaSlider(),
+    // home: EntradaRadioButton(),
     // home: EntradaCheckbox(),
     // home: CampoTexto(),
   ));  
